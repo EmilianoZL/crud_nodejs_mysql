743831ed9637a8704f6bcc6907e91ffc845aa1ba
EmilianoZL <emilianozinil@gmail.com>
2022-08-01 20:03:36 -0300

    Se agrega el proyecto

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.set('view engine','ejs')
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+
+app.use('/', require('./router'))
+
+app.listen(port, ()=>{
+    console.log('SERVER running...')
+});
